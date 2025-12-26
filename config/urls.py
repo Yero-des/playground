ab2391de868a3cfc440c68adaf80a96a6e10b174
Yero-des <yero_zc@hotmail.com>
2025-12-25 21:45:33 -0500

    Inicializacion para templates

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('messages/', include("quotes.urls"))
+    path('messages/', include("quotes.urls")),
+    path('landings/', include("landing.urls"))
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,19 @@
+from datetime import date
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    today = date.today()
+    context = {
+        "name": "Yeromi",
+        "today": today,
+        "last_name": "Zavala Castillo",
+        "languages": [
+            "python",
+            "php",
+            "c++",
+            "javascript"
+        ]
+    }
+    return render(request, "landing/landing.html", context)
