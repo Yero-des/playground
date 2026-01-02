e877f7575f910708c1f2c85076f76b2c52a37fbe
Yero-des <yero_zc@hotmail.com>
2026-01-01 22:06:38 -0500

    Sin cambios importantes

--- library/views.py
+++ library/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from library.models import Author, Book
 
 # Create your views here.
 def index(request):
+    
+    books = Book.objects.all()
+    
     return render(request, 'library/index.html', {
-        
+        'name': 'Yeromi Zavala Castillo',
+        'books': books
     })
