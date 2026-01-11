7fe05c60d8e43fe541d1be63a3a2fad8c08b7718
Yero-des <yero_zc@hotmail.com>
2026-01-11 09:13:01 -0500

    Pruebas en django admin, creacion de modelos personalizados

--- library/admin.py
+++ library/admin.py
@@ -2,9 +2,26 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookDetail, Review, Loan, Recommendation
 
 # Register your models here.
-admin.site.register(Author)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_date', 'pages')
+    search_fields = ('title', 'author__name')
+    list_filter = ('author', 'genres', 'publication_date')
+    ordering = ['-publication_date']
+    date_hierarchy = 'publication_date'
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name',)
+    list_filter = ('birth_date',)
+    ordering = ['-birth_date']
+    date_hierarchy = 'birth_date'
+
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(Book)
+# admin.site.register(Book, BookAdmin)
 admin.site.register(BookDetail)
 admin.site.register(Review)
 admin.site.register(Loan)
