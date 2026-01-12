1b9e9ad903ae8c6dcc70fba675ab460cb18adf4a
Yero-des <yero_zc@hotmail.com>
2026-01-12 09:32:02 -0500

    Actions y autocomplete

--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookDetail, Review, Loan, Recommendation
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from datetime import datetime
 
 # Register your models here.
+
+User = get_user_model()
+
+@admin.action(description="Marcar préstamos como devueltos")
+def mark_as_returned(modeladmin, request, queryset):
+    queryset.update(is_returned=True, return_date=datetime.now())
+    
+@admin.action(description="Desmarcar préstamos como no devueltos")
+def desmark_as_returned(modeladmin, request, queryset):
+    queryset.update(is_returned=False, return_date=None)
+
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
 
 class ReviewInline(admin.TabularInline):
     model = Review
@@ -16,6 +34,23 @@ class BookDetailInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = "Detalle del libro"
     
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = [LoanInline]
+    list_display = ('username', 'email')
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name',)
+    list_filter = ('birth_date',)
+    ordering = ['-birth_date']
+    date_hierarchy = 'birth_date'
+    
+@admin.register(Genre)    
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -25,19 +60,58 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'genres', 'publication_date')
     ordering = ['-publication_date']
     date_hierarchy = 'publication_date'
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'birth_date')
-    search_fields = ('name',)
-    list_filter = ('birth_date',)
-    ordering = ['-birth_date']
-    date_hierarchy = 'birth_date'
+    autocomplete_fields = ['author', 'genres']
+    
+    fieldsets = (
+        ("Información general", {
+            "fields": ('title', 'author', 'publication_date', 'genres'),
+        }),
+        ("Detalles", {
+            "fields": ('isbn', 'pages'),
+            "classes": ('collapse', )
+        }),
+    )
+    
+    
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    readonly_fields = ('loan_date', )
+    list_display = ('user', 'book', 'loan_date', 'is_returned', 'return_date')
+    ordering = ['-loan_date']
+    actions = [mark_as_returned, desmark_as_returned]
+    raw_id_fields = ['user', 'book']
+    
+    fieldsets = (
+        ("Conexiones", {
+            "fields": ('user', 'book',)
+        }),
+        ("Detalles opcionales", {
+            "fields": ('return_date', 'is_returned', 'loan_date',),
+            "classes": ('collapse',)
+        }),
+    )
+    
+@admin.register(Recommendation)
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'note')
+    search_fields = ('user__name', 'book__title')
+    date_hierarchy = 'recommended_at'
+    
+    fieldsets = (
+        ("Conexiones", {
+            "fields": ('user', 'book',)
+        }),
+        ("Nota", {
+            "fields": ('note',)
+        }),
+    )
+    
 
 # admin.site.register(Author)
-admin.site.register(Genre)
+# admin.site.register(Genre)
 # admin.site.register(Book, BookAdmin)
 admin.site.register(BookDetail)
 admin.site.register(Review)
-admin.site.register(Loan)
-admin.site.register(Recommendation)
+# admin.site.register(Loan, LoanAdmin)
+# admin.site.register(Recommendation, RecommendationAdmin)
+# admin.site.register(User, CustomUserAdmin)
