400740900b12fde8340cd6305d8a56b48871c579
Yero-des <yero_zc@hotmail.com>
2026-01-14 21:00:21 -0500

    Instalacion de nuevo formated

--- library/views.py
+++ library/views.py
@@ -1,24 +1,18 @@
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.shortcuts import render
 from library.models import Author, Book
 from django.db.models import Count, Sum, Avg
-from django.db import transaction
-from django.db import IntegrityError
 from django.contrib.auth import get_user_model
 
 # Create your views here.
 def index(request):
-    User = get_user_model()
     
     try:
-        with transaction.atomic():
-            authors = Author.objects.all().annotate(num_books=Count('books')).annotate(total_pages_books=Sum('books__pages'))
-            num_libros = Book.objects.aggregate(num_libros=Count('id'))['num_libros']
-            total_sum_pages_libros = Book.objects.aggregate(total_sum_pages_libros=Sum('pages'))['total_sum_pages_libros'] or 0
-            avg_pages = Book.objects.aggregate(avg_pages=Avg("pages"))["avg_pages"] or 0
-            avg_pages_libros = f"{avg_pages:.2f}"
-
-            # Book.objects.create(title="Nuevo Libro", author=Author.objects.get(id=1), publication_date="1994-04-06", pages=800, isbn="10293847")
+        authors = Author.objects.all().annotate(num_books=Count('books')).annotate(total_pages_books=Sum('books__pages'))
+        num_libros = Book.objects.aggregate(num_libros=Count('id'))['num_libros']
+        total_sum_pages_libros = Book.objects.aggregate(total_sum_pages_libros=Sum('pages'))['total_sum_pages_libros'] or 0
+        avg_pages = Book.objects.aggregate(avg_pages=Avg("pages"))["avg_pages"] or 0
+        avg_pages_libros = f"{avg_pages:.2f}"
 
         return render(request, 'library/index.html', {
             'name': 'Yeromi Zavala Castillo',
@@ -28,5 +22,5 @@ def index(request):
             'avg_pages_libros': avg_pages_libros
         })
         
-    except IntegrityError:        
-        return HttpResponseBadRequest("Hubo un error inesperado")
+    except Exception:
+        return HttpResponseNotFound("Pagina no encontrada")
