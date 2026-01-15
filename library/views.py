21808180b609a097f2358d0196ce23e9b8895379
Yero-des <yero_zc@hotmail.com>
2026-01-15 00:01:48 -0500

    paginado

--- library/views.py
+++ library/views.py
@@ -1,26 +1,51 @@
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.shortcuts import render
 from library.models import Author, Book
-from django.db.models import Count, Sum, Avg
+from django.db.models import Count, Sum, Avg, Q
 from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     
     try:
-        authors = Author.objects.all().annotate(num_books=Count('books')).annotate(total_pages_books=Sum('books__pages'))
-        num_libros = Book.objects.aggregate(num_libros=Count('id'))['num_libros']
-        total_sum_pages_libros = Book.objects.aggregate(total_sum_pages_libros=Sum('pages'))['total_sum_pages_libros'] or 0
-        avg_pages = Book.objects.aggregate(avg_pages=Avg("pages"))["avg_pages"] or 0
-        avg_pages_libros = f"{avg_pages:.2f}"
-
+        books = Book.objects.all().order_by('-publication_date')
+        query = request.GET.get('query_search')
+        date_start = request.GET.get('start')
+        date_end = request.GET.get('end')
+        
+        if query:
+            books = books.filter(
+                Q(title__icontains=query) | 
+                Q(author__name__icontains=query)
+            )
+            
+        if date_start and date_end:
+            books = books.filter(
+                publication_date__range=[
+                    date_start, date_end
+                ]
+            )
+            
+        paginator = Paginator(books, 8)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        
+        query_params = request.GET.copy()
+        if "page" in query_params:
+            query_params.pop('page')
+        
+        print(query_params)
+        
+        query_string = query_params.urlencode()
+        
         return render(request, 'library/index.html', {
-            'name': 'Yeromi Zavala Castillo',
-            'authors': authors,
-            'num_libros': num_libros,
-            'total_sum_pages_libros': total_sum_pages_libros,
-            'avg_pages_libros': avg_pages_libros
+            'name': 'Yeromi Zavala Castillo',            
+            'books': page_obj,
+            'query': query,
+            'query_string': query_string
         })
         
-    except Exception:
+    except Exception as e:
+        print(e)
         return HttpResponseNotFound("Pagina no encontrada")
