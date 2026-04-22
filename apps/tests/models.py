628d854b61f89341551a12aed3fcbf355498e0fb
Yero-des <yero_zc@hotmail.com>
2026-04-21 22:27:00 -0500

    Implementacion de funcionalidades basicas para videos en tests

--- apps/tests/models.py
+++ apps/tests/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.FileField(upload_to="videos", blank=True, null=True)
+    description = models.TextField()
--- apps/tests/urls.py
+++ apps/tests/urls.py
@@ -6,4 +6,5 @@ app_name = "tests"
 
 urlpatterns = [
     path('', views.TestTemplateView.as_view(), name="index"),
+    path('video/list/', views.VideoListview.as_view(), name="videos")
 ]
